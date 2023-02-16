31501a5e65f619381509fd9b379d9073afaefea9
Karol Leśniewski <lesiulesniewski@gmail.com>
2023-02-16 22:43:39 +0100

    add veriables

---
+++ style.js
@@ -0,0 +1,7 @@
+const today = new Date();
+const day = today.getDate();
+const month = today.getMonth() + 1;
+const year = today.getFullYear();
+const hour = today.getHours();
+const minuts = today.getMinutes();
+const second = today.getSeconds();
